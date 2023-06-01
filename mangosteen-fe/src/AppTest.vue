9368a0c967f925fd8fda61c587199e7a2aba089f
Mianxiong <757134184@qq.com>
2023-06-01 17:56:53 +0800

    使用TSX代替Template

---
+++ mangosteen-fe/src/AppTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ count }}</div>
+  <div>
+    <button @click="onClick">+1</button>
+  </div>
+</template>
+<script setup lang="ts">
+import { ref } from "vue";
+
+const count = ref(0);
+const onClick = () => {
+  count.value += 1;
+};
+</script>
