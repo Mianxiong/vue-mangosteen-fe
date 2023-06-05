c8b131c083eade0ef5446e81cb5ea2064d8ecbb6
Mianxiong <757134184@qq.com>
2023-06-05 19:04:20 +0800

    使用tsx代替template

--- mangosteen-fe/src/App.tsx
+++ mangosteen-fe/src/App.tsx
@@ -1,19 +1,23 @@
 import { defineComponent, ref } from "vue";
 
+// 具名导出，vscode会自动帮忙重构
 export const App = defineComponent({
     setup() {
-        const refCount = ref(0)
+        const refCount = ref(0) // 只要有ref, 使用.value , setup可以声明ref, render就不能
         const onClick = () => {
             refCount.value += 1
         }
-        return () => <>
-            <div>
-                {refCount.value}
-            </div>
+        return () => <><div>
+            {/* jsx 语法，需要自己写.vue */}
+            {refCount.value}
+        </div>
             <div>
                 <button onClick={onClick}>+1</button>
             </div>
-        </>
-
+        </> // 特殊的地方，需要用函数进行返回
     }
+    //此时，运行npm run dev，页面是空白的，它不知道怎么去渲染这个东西，需要安装vitejs/plugin-vue-jsx插件
+
+
+
 })
--- mangosteen-fe/src/AppTest.vue
+++ mangosteen-fe/src/AppTest.vue
@@ -12,3 +12,20 @@ const onClick = () => {
   count.value += 1;
 };
 </script>
+
+// <script setup lang="ts">
+// import { ref } from "vue";
+// const count = ref(0);
+
+// const onClick = () => {
+//   count.value += 1;
+// };
+// </script>
+// <template>
+//   <div>
+//     {{ count }}
+//   </div>
+//   <div>
+//     <button @click="onClick">+1</button>
+//   </div>
+// </template>
