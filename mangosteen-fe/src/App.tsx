993a149d31ea0b3ab6b15dd8a6e65379c7e0d55e
Mianxiong <757134184@qq.com>
2023-06-06 10:47:52 +0800

    feature: CSS初始化

--- mangosteen-fe/src/App.tsx
+++ mangosteen-fe/src/App.tsx
@@ -1,5 +1,6 @@
 import { defineComponent, ref } from "vue";
 import { RouterView } from "vue-router";
+import "./App.scss"
 
 // 具名导出，vscode会自动帮忙重构
 export const App = defineComponent({
@@ -19,18 +20,23 @@ export const App = defineComponent({
     // }
     //此时，运行npm run dev，页面是空白的，它不知道怎么去渲染这个东西，需要安装vitejs/plugin-vue-jsx插件
     setup() {
-        return () => <>
-            <header>导航
-                <ul>
-                    <li><router-link to="/">Foo</router-link></li>
-                    <li><router-link to="/about">Bar</router-link></li>
-                </ul>
-            </header>
+        // return () => <>
+        //     <header>导航
+        //         <ul>
+        //             <li><router-link to="/">Foo</router-link></li>
+        //             <li><router-link to="/about">Bar</router-link></li>
+        //         </ul>
+        //     </header>
+        //     <div>
+        //         <RouterView/>
+        //     </div>
+        //     <footer>页脚</footer>
+        // </>
+        return ()=> (
             <div>
                 <RouterView/>
             </div>
-            <footer>页脚</footer>
-        </>
+        )
     }
 
 
--- mangosteen-fe/src/views/Welcome.tsx
+++ mangosteen-fe/src/views/Welcome.tsx
@@ -1,9 +1,18 @@
 import { defineComponent } from 'vue';
 import { RouterView } from 'vue-router';
+import s from './Welcome.module.scss'
+import logo from '../assets/icons/mangosteen.svg'
+console.log(logo);
+
 export const Welcome = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <div><RouterView/></div>
-    )
-  }
+    setup: (props, context) => {
+        return () => (<div class={s.wrapper}>
+            <header>
+                <img src={logo} />
+                <h1>点滴记账</h1>
+            </header>
+            <main><RouterView /></main>
+        </div>
+        )
+    }
 })
