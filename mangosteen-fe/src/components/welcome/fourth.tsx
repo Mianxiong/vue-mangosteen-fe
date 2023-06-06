930ff75c2360cfe1411e246f1987c9f210b42cda
Mianxiong <757134184@qq.com>
2023-06-06 18:19:52 +0800

    优化重构

--- mangosteen-fe/src/components/welcome/fourth.tsx
+++ mangosteen-fe/src/components/welcome/fourth.tsx
@@ -1,22 +1,18 @@
-import { defineComponent } from 'vue';
-import s from './First.module.scss';
+import s from './WelcomeLayout.module.scss';
 import cloud from '../../assets/icons/cloud.svg'
 import { RouterLink } from 'vue-router';
 import { WelcomeLayout } from './WelcomeLayout';
-export const Fourth = defineComponent({
-  setup: (props, context) => {
-    return () => (
-      <WelcomeLayout>
-        {{
-          icon: () => <img class={s.icon} src={cloud} />,
-          title: () => <h2>会挣钱<br />还要会省钱</h2>,
-          buttons: () => <>
-            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-            <RouterLink to="/start">完成</RouterLink>
-            <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-          </>
-        }}
-      </WelcomeLayout>
-    )
-  }
-})
+export const Fourth = () => (
+  <WelcomeLayout>
+    {{
+      icon: () => <img class={s.icon} src={cloud} />,
+      title: () => <h2>会挣钱<br />还要会省钱</h2>,
+      buttons: () => <>
+        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+        <RouterLink to="/start">完成</RouterLink>
+        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
+      </>
+    }}
+  </WelcomeLayout>
+)
+Fourth.displayName = 'Fourth'
