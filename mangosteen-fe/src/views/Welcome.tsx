e59a8f61104e98ddd6cfe346aaae2c247215fb5e
Mianxiong <757134184@qq.com>
2023-06-08 14:37:24 +0800

    完成欢迎界面动画

--- mangosteen-fe/src/views/Welcome.tsx
+++ mangosteen-fe/src/views/Welcome.tsx
@@ -1,5 +1,5 @@
-import { defineComponent } from 'vue';
-import { RouterView } from 'vue-router';
+import { Transition, VNode, defineComponent } from 'vue';
+import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/mangosteen.svg'
 console.log(logo);
@@ -11,7 +11,18 @@ export const Welcome = defineComponent({
                 <img src={logo} />
                 <h1>点滴记账</h1>
             </header>
-            <main class={s.main}><RouterView /></main>
+            <main class={s.main}>
+                <RouterView name="main">
+                    {/* {(obj: any) => <Transition name="slide-fade"><obj.Component /></Transition>} */}
+                    {({ Component: Content, route: R }: { Component: VNode, route: RouteLocationNormalizedLoaded }) =>
+                        <Transition enterFromClass={s.slide_fade_enter_from} enterActiveClass={s.slide_fade_enter_active} leaveToClass={s.slide_fade_leave_to} leaveActiveClass={s.slide_fade_leave_active}>
+                            {Content}
+                        </Transition> 
+                    }
+                </RouterView>
+                {/* <RouterView name="main" /> */}
+            </main>
+            <footer><RouterView name="footer" /></footer>
         </div>
         )
     }
