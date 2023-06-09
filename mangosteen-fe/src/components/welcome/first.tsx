3cfa651c781f1af8b1801198788345689d7a5291
Mianxiong <757134184@qq.com>
2023-06-09 10:24:09 +0800

    完成四张欢迎卡片的切换

--- mangosteen-fe/src/components/welcome/first.tsx
+++ mangosteen-fe/src/components/welcome/first.tsx
@@ -3,27 +3,36 @@ import pig from '../../assets/icons/pig.svg'
 import { FunctionalComponent, ref, watchEffect, defineComponent } from 'vue';
 import { useSwipe } from '../../hooks/useSwipe';
 import { useRouter } from 'vue-router';
-export const First = defineComponent({
-  setup() {
-    const div = ref<HTMLDivElement>()
-    const router = useRouter() //路由器
-    const { swiping, direction } = useSwipe(div, {
-      beforeStart: e => e.preventDefault()
-    })
-    watchEffect(() => {
-      if (swiping.value && direction.value === 'left') {
-        router.push('/welcome/2')
-      }
-    })
-    return () => (<div class={s.card} ref={div}>
+// export const First = defineComponent({
+//   setup() {
+//     const div = ref<HTMLDivElement>()
+//     const router = useRouter() //路由器
+//     const { swiping, direction } = useSwipe(div, {
+//       beforeStart: e => e.preventDefault()
+//     })
+//     watchEffect(() => {
+//       if (swiping.value && direction.value === 'left') {
+//         router.push('/welcome/2')
+//       }
+//     })
+//     return () => (<div class={s.card} ref={div}>
+//       <svg>
+//         <use xlinkHref='#pig'></use>
+//       </svg>
+//       <h2>会挣钱<br />还会省钱</h2>
+//     </div>
+//     )
+//   }
+
+// })
+export const First: FunctionalComponent = () => {
+  return (
+    <div class={s.card}>
       <svg>
         <use xlinkHref='#pig'></use>
       </svg>
       <h2>会挣钱<br />还会省钱</h2>
     </div>
-    )
-  }
-
-})
-
+  )
+}
 First.displayName = 'First'
--- mangosteen-fe/src/views/Welcome.tsx
+++ mangosteen-fe/src/views/Welcome.tsx
@@ -1,17 +1,33 @@
 import { Transition, VNode, defineComponent, ref, watchEffect } from 'vue';
-import { RouteLocationNormalizedLoaded, RouterView } from 'vue-router';
+import { RouteLocationNormalizedLoaded, RouterView, useRoute, useRouter } from 'vue-router';
 import s from './Welcome.module.scss'
 import logo from '../assets/icons/mangosteen.svg'
 import { useSwipe } from '../hooks/useSwipe';
+import { throttle } from '../shared/throttle';
 console.log(logo);
 
 export const Welcome = defineComponent({
     setup: (props, context) => {
         const main = ref<HTMLElement>() // 用ref引用这个main
-        // const {direction, swiping} = useSwipe(main)
-        // watchEffect(()=>{
-        //     console.log(swiping.value, direction.value)
-        // })
+        const {direction, swiping} = useSwipe(main, {beforeStart: e => e.preventDefault()})
+        const route = useRoute() // 路由信息
+        const router = useRouter() // 路由器
+        const push = throttle(()=>{
+            if (route.name === 'Welcome1') {
+                router.push('/welcome/2')
+            } else if (route.name === 'Welcome2') {
+                router.push('/welcome/3')
+            } else if (route.name === 'Welcome3') {
+                router.push('/welcome/4')
+            } else if (route.name === 'Welcome4') {
+                router.push('/start')
+            }
+        },500)
+        watchEffect(()=>{
+            if (swiping.value && direction.value === 'left') {
+                push()
+            }
+        })
         return () => (<div class={s.wrapper}>
             <header>
                 <svg>
