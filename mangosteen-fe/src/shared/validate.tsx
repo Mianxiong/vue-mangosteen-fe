3090abfa6b00645561e26b47caed93055a8fa30d
Mianxiong <757134184@qq.com>
2023-06-12 22:25:34 +0800

    完成创建标签表单功能

---
+++ mangosteen-fe/src/shared/validate.tsx
@@ -0,0 +1,49 @@
+interface FData {
+    [k: string]: string | number | null | undefined | FData
+}
+// Rule拿到这个T，放到key上面
+type Rule<T> = {
+    // key: string
+    key: keyof T
+    message: string
+} & (
+        { type: 'required' } |
+        { type: 'pattern', regex: RegExp }
+    )
+// Rules接受这个T
+type Rules<T> = Rule<T>[]
+export type { Rule, Rules, FData}
+// const r: Rule = {
+//     key: 'name', message: 'hi', type: 'pattern', regex: /hi/
+// }
+// T 代指 FData
+// 表达的是 如果用 T 表示 formData的类型，那么 Rules 的 key 必须是 T 的 key
+export const validate = <T extends FData>(formData: T, rules: Rules<T>) => {
+    type Errors = {
+        [k in keyof T]?: string[]
+    }
+
+    const errors: Errors = {}
+    rules.map(rule => {
+        const { key, type, message } = rule
+        const value = formData[key]
+
+        switch (type) {
+            case 'required':
+                if (value === null || value === undefined || value === '') {
+                    errors[key] = errors[key] || []
+                    errors[key]?.push(message)
+                }
+                break;
+            case 'pattern':
+                if (value && !rule.regex.test(value.toString())) {
+                    errors[key] = errors[key] || []
+                    errors[key]?.push(message)
+                }
+                break;
+            default:
+                return;
+        }
+    })
+    return errors
+}
