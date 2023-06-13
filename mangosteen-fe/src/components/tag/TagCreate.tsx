a30b33bd490f3d4b6a6a1f6157306d43fb479ae3
Mianxiong <757134184@qq.com>
2023-06-13 09:25:32 +0800

    创建 TagEdit.tsx

--- mangosteen-fe/src/components/tag/TagCreate.tsx
+++ mangosteen-fe/src/components/tag/TagCreate.tsx
@@ -1,17 +1,11 @@
-import { defineComponent, PropType, reactive, toRaw } from 'vue';
-import s from './TagCreate.module.scss';
+import { defineComponent, reactive, toRaw } from 'vue';
+import s from './Tag.module.scss';
 import { Icon } from '../../shared/icon';
 import { MainLayout } from '../../layouts/MainLayout';
-import { emojiList } from '../../shared/emojiList';
 import { Button } from '../../shared/Button';
 import { EmojiSelect } from '../../shared/EmojiSelect';
 import { Rules, validate } from '../../shared/validate';
 export const TagCreate = defineComponent({
-    props: {
-        name: {
-            type: String as PropType<string>
-        }
-    },
     setup: (props, context) => {
         const formData = reactive({
             name: '',
