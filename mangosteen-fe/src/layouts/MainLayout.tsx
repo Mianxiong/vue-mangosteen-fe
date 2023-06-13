1a605a22c329973828e50563b7b02a97e9c4c894
Mianxiong <757134184@qq.com>
2023-06-13 10:45:44 +0800

    完成 TagEdit 页面的样式

--- mangosteen-fe/src/layouts/MainLayout.tsx
+++ mangosteen-fe/src/layouts/MainLayout.tsx
@@ -1,11 +1,12 @@
 import { defineComponent } from 'vue';
 import { Navbar } from '../shared/Navbar';
+import s from './MainLayout.module.scss'
 export const MainLayout = defineComponent({
     // 三个插槽，没有属性
     setup: (props, context) => {
         return () => (
-            <div>
-                <Navbar>{
+            <div class={s.wrapper}>
+                <Navbar class={s.navbar}>{
                     {
                         // default: '山竹记账', icon: <Icon name='menu'/>
                         default: () => context.slots.title?.(),
