9dc551a86ca31f4f3537f115d5807564c691300a
Mianxiong <757134184@qq.com>
2023-06-16 14:44:22 +0800

    登录成功后保存jwt至localStorage

--- mangosteen-fe/src/views/SignInPage.tsx
+++ mangosteen-fe/src/views/SignInPage.tsx
@@ -7,6 +7,7 @@ import { Button } from '../shared/Button';
 import { hasError, validate} from '../shared/validate';
 import { http } from '../shared/Http';
 import { useBool } from '../hooks/useBool';
+import { history } from '../shared/history';
 export const SignInPage = defineComponent({
     props: {
         name: {
@@ -36,7 +37,9 @@ export const SignInPage = defineComponent({
             ])
             Object.assign(errors, newErrors)
             if (!hasError(errors)) {
-                const response = await http.post('/session', formData)
+                const response = await http.post<{jwt:string}>('/session', formData)
+                localStorage.setItem('jwt', response.data.jwt)
+                history.push('/')
             }          
         }
         const onError = (error: any) => {
