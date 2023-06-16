7f6c87b12a79d3cf927d31555ca74ea80c2e2ed3
Mianxiong <757134184@qq.com>
2023-06-16 14:11:50 +0800

    修复 FormItem 没有关联 input 的 bug

--- mangosteen-fe/src/views/SignInPage.tsx
+++ mangosteen-fe/src/views/SignInPage.tsx
@@ -4,8 +4,7 @@ import { MainLayout } from '../layouts/MainLayout';
 import { Icon } from '../shared/icon';
 import { Form, FormItem } from '../shared/Form';
 import { Button } from '../shared/Button';
-import { validate } from '../shared/validate';
-import axios from 'axios';
+import { hasError, validate} from '../shared/validate';
 import { http } from '../shared/Http';
 import { useBool } from '../hooks/useBool';
 export const SignInPage = defineComponent({
@@ -25,7 +24,7 @@ export const SignInPage = defineComponent({
         })
         const refValidationCode = ref<any>()
         const {ref: refDisabled, toggle, on: disabled, off: enable} = useBool(false)
-        const onSubmit = (e: Event) => {
+        const onSubmit = async (e: Event) => {
             e.preventDefault()
             Object.assign(errors, {
                 email: [], code: []
@@ -36,6 +35,9 @@ export const SignInPage = defineComponent({
                 { key: 'code', type: 'required', message: '必填' }
             ])
             Object.assign(errors, newErrors)
+            if (!hasError(errors)) {
+                const response = await http.post('/session', formData)
+            }          
         }
         const onError = (error: any) => {
             if (error.response.status === 422) {
@@ -51,8 +53,6 @@ export const SignInPage = defineComponent({
             // 成功
             console.log('response', response);
             refValidationCode.value.startCount()
-
-
         }
         return () => (
             <MainLayout>{
@@ -61,6 +61,7 @@ export const SignInPage = defineComponent({
                     icon: () => <Icon name="left" />,
                     default: () => (
                         <div class={s.wrapper}>
+                            <div>{JSON.stringify(formData)}</div>
                             <div class={s.logo}>
                                 <Icon class={s.icon} name='mangosteen' />
                                 <h1 class={s.appName}>点滴记账</h1>
@@ -71,7 +72,7 @@ export const SignInPage = defineComponent({
                                 <FormItem ref={refValidationCode} label="验证码" type="validationCode" onClick={onClickSendValidationCode}
                                     v-model={formData.code} error={errors.code?.[0]} placeholder='请输入六位数字' disabled={refDisabled.value} countForm={60} />
                                 <FormItem style={{ paddingTop: '64px' }}>
-                                    <Button>登录</Button>
+                                    <Button type="submit">登录</Button>
                                 </FormItem>
                             </Form>
                         </div>
