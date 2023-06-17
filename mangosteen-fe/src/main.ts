70cfbb9b456931796c1184f03d7ec52eff04fc09
Mianxiong <757134184@qq.com>
2023-06-17 14:31:22 +0800

    对代码进行优化

--- mangosteen-fe/src/main.ts
+++ mangosteen-fe/src/main.ts
@@ -13,8 +13,31 @@ const router = createRouter({
 })
 // const promise = http.get('/me')
 fetchMe()
+
+const whiteList: Record<string, 'exact' | 'startsWith'> = {
+    '/': 'exact',
+    '/start': 'exact',
+    '/welcome': 'startsWith',
+    '/sign_in': 'startsWith'
+}
+
+// const whiteList = {
+//     exact: ['/', '/start'],
+//     startsWith: ['/welcome', '/sign_in']
+// }
+
 router.beforeEach(async (to, from) => {
+    // for(const key in whiteList) {
+    //     const value = whiteList[key]
+    //     if(value === 'exact' && to.path === key) {
+    //         return true
+    //     }
+    //     if(value === 'startsWith' && to.path.startsWith(key)) {
+    //         return true
+    //     }
+    // }
     if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start') {
+    // if (['/', 'start'].includes(to.path) || to.path.startsWith('/sign_in') || to.path === '/start') {
         return true
     } else {
         // await http.get('/me').catch(() => {
