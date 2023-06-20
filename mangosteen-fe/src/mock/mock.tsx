6f3ab0e0c6ffead4827171244cca7098e728b528
Mianxiong <757134184@qq.com>
2023-06-20 11:17:05 +0800

    获取 items 支出、收入、余额统计

--- mangosteen-fe/src/mock/mock.tsx
+++ mangosteen-fe/src/mock/mock.tsx
@@ -118,6 +118,11 @@ export const mockItemIndex: Mock = (config) => {
     const createBody = (n = 1, attrs?: any) => ({
         resources: createItem(n),
         pager: createPaper(page),
+        summary: {
+            income: 9900,
+            expenses: 9900,
+            balance: 0
+        }
     })
     if (!page || page === 1) {
         return [200, createBody(25)]
@@ -126,4 +131,12 @@ export const mockItemIndex: Mock = (config) => {
     } else {
         return [200, {}]
     }
+}
+
+export const mockItemIndexBalance: Mock = config => {
+    return [200, {
+        expenses: 9900,
+        income: 9900,
+        balance: 0
+    }]
 }
