cad0ce8a2492e9634f94b99df80f8dd4250992f6
Mianxiong <757134184@qq.com>
2023-06-22 14:19:06 +0800

    tag删除时支持 with_items 选项

--- mangosteen-fe/src/components/item/ItemSummary.tsx
+++ mangosteen-fe/src/components/item/ItemSummary.tsx
@@ -89,12 +89,14 @@ export const ItemSummary = defineComponent({
                 <li>
                   <div class={s.sign}>
                     {/* <span>{item.tags_id[0]}</span> */}
-                    <span>{item.tags![0].sign}</span>
+                    {/* <span>{item.tags![0].sign}</span> */}
+                    <span>{item.tags && item.tags.length > 0 ? item.tags[0].sign : '💰'}</span>
                   </div>
                   <div class={s.text}>
                     <div class={s.tagAndAmount}>
                       {/* <span class={s.tag}>{item.tags_id[0]}</span> */}
-                      <span class={s.tag}>{item.tags![0].name}</span>
+                      {/* <span class={s.tag}>{item.tags![0].name}</span> */}
+                      <span class={s.tag}>{item.tags && item.tags.length > 0 ? item.tags[0].name : '未分类'}</span>
                       <span class={s.amount}>￥<Money value={item.amount} /></span>
                     </div>
                     <div class={s.time}><Datetime value={item.happen_at} /></div>
