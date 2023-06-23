25632938d54409f6b60df88913105adcb5e2b736
Mianxiong <757134184@qq.com>
2023-06-23 14:18:55 +0800

    修复错误地请求了下一页 items 的 bug

--- mangosteen-fe/src/stores/useItemStore.tsx
+++ mangosteen-fe/src/stores/useItemStore.tsx
@@ -8,7 +8,9 @@ type State = {
 }
 type Actions = {
     reset: () => void
-    fetchItems: (startDate?: string, endDate?:string) => void
+    fetchItems: (startDate?: string, endDate?: string) => void,
+    _fetch: (firstPage: boolean, startDate?: string, endDate?: string) => void,
+    fetchNextPage: (startPage?: string, endDate?: string) => void
 }
 
 export const useItemStore = (id: string | string[]) =>
@@ -24,23 +26,34 @@ export const useItemStore = (id: string | string[]) =>
                 this.hasMore = false
                 this.page = 0
             },
-            async fetchItems(startDate, endDate) {
+            async _fetch(firstPage, startDate, endDate) {
                 if (!startDate || !endDate) {
                     return
                 }
                 const response = await http.get<Resources<Item>>('/items', {
                     happen_after: startDate,
                     happen_before: endDate,
-                    page: this.page + 1
+                    page: firstPage ? 1 : this.page + 1
                 }, {
                     _mock: 'itemIndex',
                     _autoLoading: true
                 }
                 )
                 const { resources, pager } = response.data
-                this.items?.push(...resources)
+                // this.items?.push(...resources)
+                if (firstPage) {
+                    this.items = resources
+                } else {
+                    this.items.push(...resources)
+                }
                 this.hasMore = (pager.page - 1) * pager.per_page + resources.length < pager.count
                 this.page += 1
+            },
+            async fetchNextPage(startDate, endDate) {
+                this._fetch(false, startDate, endDate)
+            },
+            async fetchItems(startDate, endDate) {
+                this._fetch(true, startDate, endDate)
             }
         }
     })
