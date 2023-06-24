99acfe0317920edf2d6ab9eec4180da915994b05
Mianxiong <757134184@qq.com>
2023-06-24 13:18:23 +0800

    UI和路由修改

--- mangosteen-fe/src/main.ts
+++ mangosteen-fe/src/main.ts
@@ -37,31 +37,35 @@ const whiteList: Record<string, 'exact' | 'startsWith'> = {
 // }
 
 router.beforeEach(async (to, from) => {
-    // for(const key in whiteList) {
-    //     const value = whiteList[key]
-    //     if(value === 'exact' && to.path === key) {
-    //         return true
-    //     }
-    //     if(value === 'startsWith' && to.path.startsWith(key)) {
-    //         return true
-    //     }
-    // }
-    if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start') {
-    // if (['/', 'start'].includes(to.path) || to.path.startsWith('/sign_in') || to.path === '/start') {
-        return true
-    } else {
-        // await http.get('/me').catch(() => {
-        // const path = await promise.catch(() => {
-        //     return '/sign_in?return_to=' + to.path
-        // })
-        // return true
-        // const path = await mePromise!.then(
-        const path = await mePromise!.value!.then(
-            () => true, // 成功
-            () => '/sign_in?return_to=' + from.path //失败
-        )
-        return path
+    for(const key in whiteList) {
+        const value = whiteList[key]
+        if(value === 'exact' && to.path === key) {
+            return true
+        }
+        if(value === 'startsWith' && to.path.startsWith(key)) {
+            return true
+        }
     }
+    return mePromise!.value!.then(
+        () => true,
+        () => '/sign_in?return_to=' + from.path
+    )
+    // if (to.path === '/' || to.path.startsWith('/welcome') || to.path.startsWith('/sign_in') || to.path === '/start') {
+    // // if (['/', 'start'].includes(to.path) || to.path.startsWith('/sign_in') || to.path === '/start') {
+    //     return true
+    // } else {
+    //     // await http.get('/me').catch(() => {
+    //     // const path = await promise.catch(() => {
+    //     //     return '/sign_in?return_to=' + to.path
+    //     // })
+    //     // return true
+    //     // const path = await mePromise!.then(
+    //     const path = await mePromise!.value!.then(
+    //         () => true, // 成功
+    //         () => '/sign_in?return_to=' + from.path //失败
+    //     )
+    //     return path
+    // }
 })
 
 
